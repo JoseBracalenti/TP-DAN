825df5c8a67bca51df31b9ddaa5946a8205ca06d
JoseBracalenti <josebracalenti@hotmail.com>
2025-06-11 19:00:58 -0300

    ms-pedidos aparece en eureka

--- ms-pedidos/src/main/java/isi/dan/ms/pedidos/MsPedidosApplication.java
+++ ms-pedidos/src/main/java/isi/dan/ms/pedidos/MsPedidosApplication.java
@@ -3,7 +3,9 @@ package isi.dan.ms.pedidos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsPedidosApplication {
 
 	public static void main(String[] args) {
